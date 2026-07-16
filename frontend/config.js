549065c9fb4ae11a2d439c51967745ae86506063
naiksinchana61-ops <naiksinchana61@gmail.com>
2026-07-16 11:01:01 +0530

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // ===== BACKEND CONFIGURATION =====
 // Change this to your deployed Render backend URL after deploying it.
 // Example: "https://smart-complaint-backend.onrender.com"
-const BACKEND_URL = "https://smart-complaint-tracker.onrender.com";
+const BACKEND_URL = "https://smart-complaint-tracker-fkte.onrender.com";
